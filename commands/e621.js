44c7e40233bf0c39b1a285c80edf47999eb34707
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2019-03-23 14:30:52 +0000

    Updated e621
    e621 commands should now work! Usage: <prefix>e621 <tag>

--- commands/e621.js
+++ commands/e621.js
@@ -2,42 +2,40 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const Request = require("request");
 const snekfetch = require('snekfetch');
-var fs = require('fs');
 const Command = require("../base/Command.js");
 
 class e621 extends Command {
   constructor (client) {
     super(client, {
       name: "e621",
-      description: "Grabs an image from e621 (Requires NSFW channel)",
+      description: "e621 test",
       usage: "e621 <tag>",
       aliases: ["e621"],
-	  category: "NSFW commands"
     });
   }
-
-  async run (message, args, level) {
+  
+  async run (message, args, level) { // eslint-disable-line no-unused-vars
     try {
-		
+
 const lewd = [
-`Ooooo~ Thats hot!`,
-`Lewd!`,
-`e621s finest~`,
-`Spicy!`,
-`Yum ;3`,
-`Keep them cumming!`,
-`nnf~`,
-`Ohhhh yes~`,
-`Hehe~`,
-`Lewd~ ;3`,
-`>//////<`,
-`^////^`,
-`I like this one! ^///^`,
-`Mmmmf~`,
-`Ohhh yeah, thats the stuff!`,
+'Ooooo~ Thats hot!',
+'Lewd!',
+'e621s finest~',
+'Spicy!',
+'Yum ;3',
+'Keep them cumming!',
+'nnf~',
+'Ohhhh yes~',
+'Hehe~',
+'Lewd~ ;3',
+'>//////<',
+'^////^',
+'I like this one! ^///^',
+'Mmmmf~',
+'Ohhh yeah, thats the stuff!',
  ]
- 
-function randomval(Min, Max){
+
+ function randomval(Min, Max){
   return Math.floor( Math.random() * ( 1 + Max - Min ) ) + Min;
 }
 
@@ -45,15 +43,14 @@ function EmbedSend(Str,Url,Mess){
   Mess.channel.send({embed:{
     color: 3447003,
     author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
+      name: "Iris",
+      icon_url: "https://i.imgur.com/rRVy516.jpg"
     },
     description: Str,
     image:{url: Url}
   }});
 }
 
-//E6 #####################################################################################
 async function E621Lewd(Args,Mess){
     const BaseE6Url = "https://e621.net/post/index.json?&limit=15&tags=order:random+";
     var BaseTextUrl = BaseE6Url + Args.join("+");
@@ -78,25 +75,10 @@ async function E621Lewd(Args,Mess){
             }
           }
     );
-
-
-function EmbedSend(Str,Url,Mess){
-	    let embed = new Discord.RichEmbed()
-		.setAuthor("e621","https://i.imgur.com/rRVy516.jpg")
-		.setColor(3447003)
-		.setImage(lewd[Math.floor(Math.random() * lewd.length)],PURL,Mess)
-		.setFooter("©Toby#0621 2019", "https://i.imgur.com/dJb2tIq.png")
-		message.channel.send({embed});
 }
-
-
-if (message.channel.nsfw === false) {
-			return message.reply("⚠ Not an NSFW channel :c");
-		}
-		E621Lewd(args,message); 
-		
-		}
-	}catch (e) {
+    E621Lewd(args,message); 
+ 
+     } catch (e) {
       console.log(e);
     }
   }
