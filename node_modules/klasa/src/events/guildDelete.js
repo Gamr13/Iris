9868708b956c3f5a11a2d5d198393122021a95c1
jpitkanen18 <jpitkanen18@gmail.com>
2019-12-23 19:29:03 +0200

    Deleted node_modules

--- node_modules/klasa/src/events/guildDelete.js
+++
@@ -1,9 +0,0 @@
-const { Event } = require('klasa');
-
-module.exports = class extends Event {
-
-	run(guild) {
-		if (guild.available) this.client.settings.guilds.destroy(guild.id).catch(() => null);
-	}
-
-};
--- node_modules/klasa/src/events/messageUpdate.js
+++
@@ -1,9 +0,0 @@
-const { Event } = require('klasa');
-
-module.exports = class extends Event {
-
-	run(old, msg) {
-		if (old.content !== msg.content && this.client.config.cmdEditing) this.client.emit('message', msg);
-	}
-
-};
